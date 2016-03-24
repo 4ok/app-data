9e76e3f37e8f690210650d75217b6a2ef37dcd99
lipolyakov <lipolyakov@yandex-team.ru>
2016-03-25 00:39:08 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = class {
+
+    callMethod(params, args) {
+        params           = params.split('/');
+        const controller = params[0];
+        const action     = params[1] || 'index';
+
+        return this._callController(controller, action, args);
+    }
+
+    _callController(controllerName, actionName, args) {
+        const controllersPath = './controllers/' + controllerName;
+        const Controller      = require(controllersPath);
+        const controller      = new Controller();
+
+        return controller[actionName + 'Action'](args);
+    }
+};
